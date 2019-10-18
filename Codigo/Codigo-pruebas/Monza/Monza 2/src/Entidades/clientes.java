ec18e77b2c24e2440fac4bf46098ef48a434a033
DiegoRS <sibace_sa@hotmail.com>
2019-10-18 13:54:59 -0500

    upload
    codigo

---
+++ Codigo/Codigo-pruebas/Monza/Monza 2/src/Entidades/clientes.java
@@ -0,0 +1,12 @@
+package Entidades;
+
+public class clientes extends notas {
+
+    private String nombre;
+
+    private String direccion;
+
+    private String idcliente;
+
+    private int telefono;
+}
---
+++ Codigo/Codigo-pruebas/Monza/Monza 2/src/Entidades/info_productos.java
@@ -0,0 +1,12 @@
+package Entidades;
+
+public class info_productos extends Prodcutos {
+
+    private int existencia;
+
+    private String categoria;
+
+    private int bandera;
+
+    private String imagen;
+}
