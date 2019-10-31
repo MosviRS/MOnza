e79e0df0173729135c4e46a4f7e084b938326f52
Cesar Pedraza Hernandez <Raseck58@live.com.mx>
2019-10-30 20:12:23 -0600

    Edit 2

--- Codigo/Codigo-pruebas/Monza/Monza 2/src/metodos/SqlUsuarios.java
+++ Codigo/Codigo-pruebas/Monza/Monza 2/src/metodos/SqlUsuarios.java
@@ -11,14 +11,17 @@ import javax.swing.JOptionPane;
 
 public class SqlUsuarios extends Conexion {
     
-    public boolean registrar(Usuario usr) {
-        PreparedStatement ps = null;
-        Connection con;
-        con= ManipulaDBC.conectaDB();
-
+    public boolean registrar(Usuario usr) {//Metodo de registro recibe un Usuario
+        PreparedStatement ps = null; //Objeto que permite usar declaraciones preparadas por la libreria para un facil acceso a MySql
+        Connection con;//objeto que permite la conexion a la BD(MySql)
+        con= ManipulaDBC.conectaDB();//Objeto que permite manipular a la BD
+        //Codigo MySql para insertar Registros, guardado en una Variable de tipo String
+        //para registrar en la Tabla Usuarios de la BD, previamente diseñada
         String sql = "INSERT INTO usuario (idcuenta, nombre, tipo, Apaterno, Amaterno, password, tipo_pregunta, respuesta, correo) VALUES(?,?,?,?,?,?,?,?,?)";
         try {
-            ps = con.prepareStatement(sql);            
+            ps = con.prepareStatement(sql);//Precarga declaraciondes por default de MySql
+            //Inserta nuevos campos a la Tabla Usuarios
+            //Numero de busqueda,Datos a Ingresar;
             ps.setString(1, usr.getUser());
             ps.setString(2, usr.getNombreUser());
             ps.setInt(3, usr.getTipo());
@@ -29,34 +32,35 @@ public class SqlUsuarios extends Conexion {
             ps.setString(8, usr.getRespuesta());
             ps.setString(9, usr.getCorreo());
             ps.execute();
-            return true;
+            return true; // Retorna una comprobacion si el registro fue exitoso
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.toString());
+            JOptionPane.showMessageDialog(null, e.toString());//Mensajes de Error 
             return false;
         } finally {
             try {
-                ManipulaDBC.desconectaDB(con);
+                ManipulaDBC.desconectaDB(con);//Libera conexion a la BD
             } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.toString());
+                JOptionPane.showMessageDialog(null, e.toString());//Mas Mensajes de Error
             }
         }
     }
 
-    public boolean login(Usuario usr) {
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Connection con;
-        con= ManipulaDBC.conectaDB();
-
+    public boolean login(Usuario usr) {//Funcion para comprobacion de Usuario, recibe un Usuario en una variable de Tipo String
+        PreparedStatement ps = null;//Objeto que permite usar declaraciones preparadas por la libreria para un facil acceso a MySql
+        ResultSet rs = null;//Muestra Datos almacenados en la BD
+        Connection con;//objeto que permite la conexion a la BD(MySql)
+        con= ManipulaDBC.conectaDB();//Objeto que permite manipular a la BD
+        //Codigo MySql para Seleccionar(o bien Buscar) Registros, guardado en una Variable de tipo String
+        //para seleccionar elementos en la Tabla Usuarios de la BD, previamente diseñada
         String sql = "SELECT idcuenta,nombre, tipo, Apaterno, Amaterno, password, tipo_pregunta, respuesta, correo FROM usuario WHERE idcuenta =?";
         try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, usr.getUser());
-            rs = ps.executeQuery();
+            ps = con.prepareStatement(sql);//Precarga declaraciondes por default de MySql
+            ps.setString(1, usr.getUser());//Busqueda de usuario
+            rs = ps.executeQuery();//Muestra el Resultado del usuario  encontrado
             
-            if (rs.next()) {
-                if (usr.getPassword().equals(rs.getString(6))) {                                        
-                    
+            if (rs.next()) {//muestra todos los datos del usuario encontrado
+                if (usr.getPassword().equals(rs.getString(6))) {//Comprueba que el password sea correcto y coicida con el usuario                                        
+                    //Envia datos del Usuario ubicado a la clase entidad Usuarios 
                     usr.setNombreUser(rs.getString(2));
                     usr.setTipo(rs.getInt(3));
                     usr.setApaterno(rs.getString(4));
@@ -65,7 +69,7 @@ public class SqlUsuarios extends Conexion {
                     usr.setRespuesta(rs.getString(8));
                     usr.setCorreo(rs.getString(9));
                     
-                    return true;
+                    return true;// Retorna una comprobacion de la existencia del usuario en la BD 
                 } else {
                     return false;
                 }
@@ -73,27 +77,31 @@ public class SqlUsuarios extends Conexion {
 
             return false;
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.toString());
+            JOptionPane.showMessageDialog(null, e.toString());//Mensajes de Error
             return false;
         } finally {
             try {
-                con.close();
+                con.close();//libera conexion de la BD
             } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, e.toString());
+                JOptionPane.showMessageDialog(null, e.toString());//Mas mensajes de error 
             }
         }
+        
     }
     public boolean Olvide(Usuario usr){
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Connection con;
-        con= ManipulaDBC.conectaDB();
+        PreparedStatement ps = null;//Objeto que permite usar declaraciones preparadas por la libreria para un facil acceso a MySql
+        ResultSet rs = null;//Muestra Datos almacenados en la BD
+        Connection con;//objeto que permite la conexion a la BD(MySql)
+        con= ManipulaDBC.conectaDB();//objeto que permite la conexion a la BD(MySql)
+        //Codigo MySql para Seleccionar(o bien Buscar) Registros, guardado en una Variable de tipo String
+        //para seleccionar elementos en la Tabla Usuarios de la BD, previamente diseñada
         String sql = "SELECT idcuenta,nombre, tipo, Apaterno, Amaterno, password, tipo_pregunta, respuesta, correo FROM usuario WHERE idcuenta =?";
         try{
-            ps = con.prepareStatement(sql);
-            ps.setString(1, usr.getUser());
-            rs = ps.executeQuery();
+            ps = con.prepareStatement(sql);//Precarga declaraciondes por default de MySql
+            ps.setString(1, usr.getUser());//Busqueda de usuario
+            rs = ps.executeQuery();//Muestra el Resultado del usuario  encontrado
             if(rs.next()){
+                //toma datos del usuario encontrado y los envia a la Clase Entidad Usuarios
                 usr.setUser(rs.getString(1));
                 usr.setNombreUser(rs.getString(2));
                 usr.setTipo(rs.getInt(3));
@@ -103,7 +111,7 @@ public class SqlUsuarios extends Conexion {
                 usr.setPregunta(rs.getInt(7));
                 usr.setRespuesta(rs.getString(8));
                 usr.setCorreo(rs.getString(9));
-                return true;                
+                return true;//Retorna una comprobacion                 
             }
             return false;
             
@@ -120,21 +128,22 @@ public class SqlUsuarios extends Conexion {
             
         
     }
-    public int existeUsuario(String usuario) {
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Connection con;
-        con= ManipulaDBC.conectaDB();
-
+    public int existeUsuario(String usuario) {//Comprobacion de existencia del Usuario
+        PreparedStatement ps = null;//Objeto que permite usar declaraciones preparadas por la libreria para un facil acceso a MySql
+        ResultSet rs = null;//Muestra Datos almacenados en la BD
+        Connection con;//objeto que permite la conexion a la BD(MySql)
+        con= ManipulaDBC.conectaDB();//objeto que permite la conexion a la BD(MySql)
+        //Seleccionar Contar idcuenta  de Tabla Usuario, donde Idcuenta es igual a...
+        // Codigo de MySql para buscar Idcuenta en la tabla Usuario
         String sql = "SELECT count(idcuenta) FROM usuario WHERE idcuenta = ?";
 
         try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, usuario);
-            rs = ps.executeQuery();
+            ps = con.prepareStatement(sql);//Precarga declaraciondes por default de MySql
+            ps.setString(1, usuario);//Busqueda de usuario
+            rs = ps.executeQuery();//Muestra resultado de la busqueda
 
             if (rs.next()) {
-                return rs.getInt(1);
+                return rs.getInt(1);//Retorna ID
             }
 
             return 1;
@@ -144,7 +153,7 @@ public class SqlUsuarios extends Conexion {
             return 1;
         } finally {
             try {
-                con.close();
+                con.close();//Libera conexion
             } catch (SQLException e) {
                 JOptionPane.showMessageDialog(null, e.toString());
             }
